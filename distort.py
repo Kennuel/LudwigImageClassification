ddf68c5aebb051d1c2dc678aa8ab7b3996401288
hewi <h.wiechula@it-frankfurt.com>
2019-06-22 23:22:27 +0200

    Added Data to set

--- distort.py
+++ distort.py
@@ -1,21 +1,17 @@
 import Augmentor
-l = Augmentor.Pipeline("./like_orig/new")
-d = Augmentor.Pipeline("./dislike_orig/new")
+l = Augmentor.Pipeline("./like_orig/")
+d = Augmentor.Pipeline("./dislike_orig/")
 
-l.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
-l.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
-
-d.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
-d.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
-
-d.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=8)
-d.flip_left_right(probability=0.5)
-d.flip_top_bottom(probability=0.5)
-
-l.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=8)
+l.rotate(probability=0.7, max_left_rotation=25, max_right_rotation=25)
+l.zoom(probability=0.5, min_factor=1.1, max_factor=1.75)
+l.random_distortion(probability=0.5, grid_width=4, grid_height=4, magnitude=8)
 l.flip_left_right(probability=0.5)
 l.flip_top_bottom(probability=0.5)
+l.sample(3657)
 
-
-l.sample(5000)
-d.sample(5000)
+d.rotate(probability=0.7, max_left_rotation=25, max_right_rotation=25)
+d.zoom(probability=0.5, min_factor=1.1, max_factor=1.75)
+d.random_distortion(probability=0.5, grid_width=4, grid_height=4, magnitude=8)
+d.flip_left_right(probability=0.5)
+d.flip_top_bottom(probability=0.5)
+# d.sample(4908)
